dec82a0f22214b8c1a328551d263b2682b97eb45
Yulong Tan <swifties@uw.edu>
2016-05-14 00:04:30 -0700

    added button

--- countries.php
+++ countries.php
@@ -1,24 +1,26 @@
 <html>
-<head>
-	<meta charset="UTF-8">
-	<title>Country Data</title>
-	<link rel="stylesheet" href="countries.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-</head>
-<body>
-	<div id="container">
-		<div class="main bg-success form-group has-success">
-			<h1>Country Data</h1>
-			<form action="countries-pop" method="get"></form>
-			<div>
-				<input id="country-field" name="country" type="text" placeholder="type country name" autofocus="autofocus">
-				<input id="submit" type="submit" value="Get Population">
-			</div>
-			<div id="display">
-				<h2>This is some placeholder text</h2>
+	<head>
+		<meta charset="UTF-8">
+		<title>Country Data</title>
+		<link rel="stylesheet" href="countries.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	</head>
+	<body>
+		<div id="container">
+			<div class="main bg-success form-group has-success">
+				<h1>Country Data</h1>
+				<form action="countries-pop" method="get"></form>
+				<div>
+					<input id="country-field" name="country" type="text" placeholder="type country name" autofocus="autofocus">
+					<input id="submit" type="submit" value="Get Population">
+					<button id="clear">Clear</button>
+				</div>
+				<div id="display">
+					<h2 id="message">This is some placeholder text</h2>
+				</div>
 			</div>
 		</div>
-	</div>
-</body>
+	</body>
+	<script src="countries.js"></script>
 </html>
