6e8c1bccff5286596a6ee9c9b668311736a338e2
Yulong Tan <swifties@uw.edu>
2016-05-13 23:53:57 -0700

    used div for placeholder text

--- countries.php
+++ countries.php
@@ -15,7 +15,9 @@
 				<input id="country-field" name="country" type="text" placeholder="type country name" autofocus="autofocus">
 				<input id="submit" type="submit" value="Get Population">
 			</div>
-			<h2 id="display">This is some placeholder text</h2>
+			<div id="display">
+				<h2>This is some placeholder text</h2>
+			</div>
 		</div>
 	</div>
 </body>
