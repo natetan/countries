4dc30d0863af3e9c9a47ce0f41850cdd0581ab0b
Yulong Tan <swifties@uw.edu>
2016-05-13 23:20:32 -0700

    first commit

---
+++ countries.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Country Data</title>
+	<link rel="stylesheet" href="countries.css">
+</head>
+<body>
+	<div id="container">
+		<div id="main">
+			<h1>Country Data</h1>
+			<input type="text">
+			
+		</div>
+	</div>
+</body>
+</html>
