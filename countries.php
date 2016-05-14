8592aa6f33ab215dc4ac93183a89bae8edb33e45
Yulong Tan <swifties@uw.edu>
2016-05-14 00:20:39 -0700

    fixed form

--- countries.php
+++ countries.php
@@ -10,12 +10,13 @@
 		<div id="container">
 			<div class="main bg-success form-group has-success">
 				<h1>Country Data</h1>
-				<form action="countries-pop" method="get"></form>
-				<div>
-					<input id="country-field" name="country" type="text" placeholder="type country name" autofocus="autofocus">
-					<input id="submit" type="submit" value="Get Population">
-					<button id="clear">Clear</button>
-				</div>
+				<form action="countries-pop" method="get">
+					<div>
+						<input id="country-field" name="country" type="text" placeholder="type country name" autofocus="autofocus">
+						<input id="submit" type="submit" value="Get Population">
+						<button id="clear">Clear</button>
+					</div>
+				</form>
 				<div id="display">
 					<h2 id="message">This is some placeholder text</h2>
 				</div>
