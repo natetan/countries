8733b4c81f4eb802b2c7ee4466a38055ea5448f0
Yulong Tan <swifties@uw.edu>
2016-05-13 23:38:32 -0700

    added bootstrap

--- countries.php
+++ countries.php
@@ -3,10 +3,12 @@
 	<meta charset="UTF-8">
 	<title>Country Data</title>
 	<link rel="stylesheet" href="countries.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 </head>
 <body>
 	<div id="container">
-		<div id="main">
+		<div id="main bg-success">
 			<h1>Country Data</h1>
 			<form action="countries-pop" method="get"></form>
 			<div>
